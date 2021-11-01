c1ddfd03eaa1a5be059e6a83e8986b8f27c1bd4a
Wearme <31043397+Wearme@users.noreply.github.com>
2021-11-02 04:36:54 +0900

    beta

--- p-ffmpeg.sh
+++ p-ffmpeg.sh
@@ -1,9 +1,32 @@
 #!/bin/bash
 
-
+IFS=$'\n'
 
 declare -a target_format=("wav" "flac" "wma")
-declare -a convert_to_format=("m4a" "mp3" "mwa")
+declare -a convert_to_format=("m4a" "mp3" "mwa" "aac" "wav")
+
+declare default_option="-vn"
+
+declare -A format_m4a=(
+	["acodec"]="aac"
+	["format"]="mp4"
+)
+declare -A format_aac=(
+	["acodec"]="aac"
+	["format"]="mp4"
+)
+declare -A format_wav=(
+	["acodec"]="pcm_s16le"
+	["format"]="wav"
+)
+declare -A format_mp3=(
+	["acodec"]="libmp3lame"
+	["format"]="mp3"
+)
+declare -A format_wma=(
+	["acodec"]="wav2"
+	["format"]="asf"
+)
 
 arg_check()
 {
@@ -91,5 +114,96 @@ flag_check $bConvert $1 "Input the converted extension"
 
 # Get Current dir file
 
+case $2 in
+"m4a" )
+	declare -g selected_codec="${format_m4a["acodec"]}"
+	declare -g selected_format="${format_m4a["format"]}"
+	;;
+"aac" )
+	declare -g selected_codec="${format_aac["acodec"]}"
+	declare -g selected_format="${format_aac["format"]}"
+	;;
+"wav" )
+	declare -g selected_codec="${format_wav["acodec"]}"
+	declare -g selected_format="${format_wav["format"]}"
+	;;
+"mp3" )
+	declare -g selected_codec="${format_mp3["acodec"]}"
+	declare -g selected_format="${format_mp3["format"]}"
+	;;
+"wma" )
+	declare -g selected_codec="${format_wma["acodec"]}"
+	declare -g selected_format="${format_wma["format"]}"
+	;;
+* )
+	echo "Why come here"
+	exit 1
+	;;
+esac
+
+# echo "DEBUG : $selected_codec $selected_format"
+
+declare -a command_ls=($(ls *.$1 2>/dev/null | sed 's/\.[^\.]*$//' 2>/dev/null))
+
+if [ "${#command_ls[@]}" == 0 ]
+then
+	echo "\"$1\" file not found"
+	exit 1
+fi
 
 
+# Ready Directory
+
+declare -a already_exist=()
+
+bDirectory="$3"
+if [ -d $bDirectory ]
+then
+	for tmp_exist_file in "$3/${command_ls[@]}.$2"
+	do
+		if [ -e $tmp_exist_file ]
+		then
+			already_exist+=("$tmp_exist_file")
+		fi
+	done
+else
+	mkdir $3
+fi
+
+if [ ! "${#already_exist[@]}" == 0 ]
+then
+	echo "Some files already exist."
+	for ef in "${already_exist[@]}"
+	do
+		echo "    $ef"
+	done
+	read -p "Remove them? (y/N): " response
+
+	case "$response" in
+	[yY]*)
+		echo "Remove"
+		;;
+	* )
+		echo "Aborted."
+		exit
+		;;
+	esac
+fi
+
+# Converting
+
+for file_name in "${command_ls[@]}"
+do
+	# echo "DEBUG : \"${file_name}.$1\" \"$3${file_name}.$2\""
+	try_path="$3/${file_name}.$2"
+	ffmpeg $default_option -i "${file_name}.$1" -acodec $selected_codec -f $selected_format $try_path 2>/dev/null
+
+	if [ $? == 0 ]
+	then
+		echo "success : $try_path"
+	else
+		echo "Failed : $try_path"
+	fi
+done
+
+echo "Done"
